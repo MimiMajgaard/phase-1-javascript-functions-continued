8871fb7607bed703ba12b757b99546535a76e768
Mimi Majgaard <mimimajgaard@gmail.com>
2022-03-22 16:56:35 -0700

    commit tree 51d4364b42e7d480444323c7a991be9b8f50c3bd

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate'){
+        return `This Saturday, I want to ${activity}!`;
+   
+        
+}
+
+
+
+let mondayWork = function(activity = 'go to the office'){
+        return `This Monday, I will ${activity}.`;
+    
+}
+
+
+
+function wrapAdjective(specialChar = '*'){
+    return function(text = 'special'){
+        return `You are ${specialChar}${text}${specialChar}!`;
+    }
+}
+
+//function wrapAdjective(){
+    //const innerFunction = function (name, lang = "Python") {
+        //return `${greeting}, ${name}! ${msg} ${lang}`;
+    //};
+    //return innerFunction("student", "JavaScript");
+//}
+//outer("Hello")("student", "JavaScript");
